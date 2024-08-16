48a39906340f52cac9506ba93b1bf681054b2190
KNU-K <knukang334@gmail.com>
2024-08-16 23:36:39 +0900

    init : 초기 구성 파일 업로드

---
+++ src/test/java/com/tasksprints/auction/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasksprints.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
