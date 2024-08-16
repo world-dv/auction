17d36feaae31afe283805f3cdd172ce45f8d92aa
KNU-K <knukang334@gmail.com>
2024-08-17 03:27:49 +0900

    refactor : Controller 에서 ResponseEntity 를 통해서 반환 되도록 변경

--- src/main/java/com/tasksprints/auction/domain/user/controller/UserController.java
+++ src/main/java/com/tasksprints/auction/domain/user/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.tasksprints.auction.domain.user.controller;
 
+import com.tasksprints.auction.common.ApiResponse;
+import com.tasksprints.auction.common.ApiResponseMessages;
+import com.tasksprints.auction.domain.user.dto.UserDetailsDTO;
 import com.tasksprints.auction.domain.user.dto.UserRequest;
-import com.tasksprints.auction.domain.user.model.User;
+import com.tasksprints.auction.domain.user.dto.UserSummaryDTO;
 import com.tasksprints.auction.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -12,32 +18,33 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
-    /**
-     *
-     * @return User
-     * 이부분은 추후에 Response DTO로 교체할 예정
-     */
     @PostMapping()
-    User register(@RequestBody UserRequest.Register user){
-        return userService.createUser(user);
+    public ResponseEntity<ApiResponse<UserDetailsDTO>> register(@RequestBody UserRequest.Register user) {
+        UserDetailsDTO newUser = userService.createUser(user);
+        return ResponseEntity.ok(ApiResponse.success(ApiResponseMessages.USER_CREATED_SUCCESS, newUser));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<UserDetailsDTO>> getUserById(@PathVariable Long id) {
+        UserDetailsDTO user = userService.getUserDetailsById(id);
+        return ResponseEntity.ok(ApiResponse.success(null, user));
+    }
 
     @GetMapping()
-    User getUserById(@RequestParam Long id){
-        return userService.getUserById(id);
+    public ResponseEntity<ApiResponse<List<UserSummaryDTO>>> getAllUsers() {
+        List<UserSummaryDTO> users = userService.getUsersSummary();
+        return ResponseEntity.ok(ApiResponse.success(null, users));
     }
 
-
     @PutMapping()
-    User updateUser(@RequestParam Long id, @RequestBody UserRequest.Update user){
-        return userService.updateUser(id, user);
+    public ResponseEntity<ApiResponse<UserDetailsDTO>> updateUser(@RequestParam Long id, @RequestBody UserRequest.Update user) {
+        UserDetailsDTO updatedUser = userService.updateUser(id, user);
+        return ResponseEntity.ok(ApiResponse.success(ApiResponseMessages.USER_UPDATED_SUCCESS, updatedUser));
     }
 
     @DeleteMapping()
-    String deleteUser(@RequestParam Long id){
+    public ResponseEntity<ApiResponse<String>> deleteUser(@RequestParam Long id) {
         userService.deleteUser(id);
-        return "good";
+        return ResponseEntity.ok(ApiResponse.success(ApiResponseMessages.USER_DELETED_SUCCESS));
     }
-
 }
