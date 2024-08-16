1927c5849e6650faf60c13961c7abb782e133ad6
KNU-K <knukang334@gmail.com>
2024-08-17 04:20:14 +0900

    test : JpaMetamodelMappingContext 추가

--- src/test/java/com/tasksprints/auction/domain/user/UserControllerTest.java
+++ src/test/java/com/tasksprints/auction/domain/user/UserControllerTest.java
@@ -15,6 +15,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
+@MockBean(JpaMetamodelMappingContext.class)
 class UserControllerTest {
 
     @Autowired
