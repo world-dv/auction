a152f440b75f98e64df28f4840d6d2434580d46f
KNU-K <knukang334@gmail.com>
2024-09-04 12:23:47 +0900

    fix : QueryDSL 을 사용함으로써, 테스트 내에 QueryDSL 주입

--- src/test/java/com/tasksprints/auction/domain/product/ProductRepositoryTest.java
+++ src/test/java/com/tasksprints/auction/domain/product/ProductRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.tasksprints.auction.domain.product;
 
+import com.tasksprints.auction.common.config.QueryDslConfig;
 import com.tasksprints.auction.domain.auction.model.Auction;
 import com.tasksprints.auction.domain.auction.model.AuctionCategory;
 import com.tasksprints.auction.domain.auction.model.AuctionStatus;
@@ -13,6 +14,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -23,6 +25,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
+@Import(QueryDslConfig.class)
 public class ProductRepositoryTest {
 
     @Autowired
