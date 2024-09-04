67118270a4bcb64da60489bdbd45623a893afb72
world-dv <goodforx0147@naver.com>
2024-09-05 01:01:24 +0900

    delete: @EnableJpaAuditing을 제거합니다.

--- src/main/java/com/tasksprints/auction/common/config/JpaAuditingConfig.java
+++ src/main/java/com/tasksprints/auction/common/config/JpaAuditingConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
+//@EnableJpaAuditing 제거 -> 프로젝트 선언부에 이미 선언되어 있음
 public class JpaAuditingConfig {
 }
