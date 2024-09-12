bd3959d11c0e59066bb2d200d38bf783d11b1905
KNU-K <knukang334@gmail.com>
2024-09-13 01:25:18 +0900

    refactor : sortBy에 따라 OrderSpecifier 로 정렬이 가능하게 수정

--- src/main/java/com/tasksprints/auction/domain/auction/repository/support/AuctionCriteriaRepository.java
+++ src/main/java/com/tasksprints/auction/domain/auction/repository/support/AuctionCriteriaRepository.java
@@ -2,14 +2,9 @@ package com.tasksprints.auction.domain.auction.repository.support;
 
 import com.tasksprints.auction.domain.auction.dto.request.AuctionRequest;
 import com.tasksprints.auction.domain.auction.model.Auction;
-import com.tasksprints.auction.domain.auction.model.AuctionStatus;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface AuctionCriteriaRepository {
     List<Auction> getAuctionsByFilters(AuctionRequest.SearchCondition searchCondition);
-
-    //삭제해야 함
-    List<Auction> getAuctionsEndWith24Hours(LocalDateTime now, LocalDateTime next24Hours, AuctionStatus auctionStatus);
 }
--- src/main/java/com/tasksprints/auction/domain/auction/repository/support/AuctionCriteriaRepositoryImpl.java
+++ src/main/java/com/tasksprints/auction/domain/auction/repository/support/AuctionCriteriaRepositoryImpl.java
@@ -1,17 +1,15 @@
 package com.tasksprints.auction.domain.auction.repository.support;
 
 import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.tasksprints.auction.domain.auction.dto.request.AuctionRequest;
 import com.tasksprints.auction.domain.auction.model.Auction;
-import com.tasksprints.auction.domain.auction.model.AuctionStatus;
 import com.tasksprints.auction.domain.auction.model.QAuction;
 import com.tasksprints.auction.domain.product.model.QProduct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -23,49 +21,51 @@ public class AuctionCriteriaRepositoryImpl implements AuctionCriteriaRepository 
         QAuction auction = QAuction.auction;
         QProduct product = QProduct.product;
 
+        BooleanBuilder builder = buildSearchCondition(condition, auction, product);
+
+        var query = queryFactory.selectFrom(auction)
+            .leftJoin(auction.product, product)
+            .where(builder);
+
+        OrderSpecifier<?> sortOrder = getSortOrder(condition, auction);
+        if (sortOrder != null) {
+            query = query.orderBy(sortOrder);
+        }
+
+        return query.fetch();
+    }
+
+    private BooleanBuilder buildSearchCondition(AuctionRequest.SearchCondition condition, QAuction auction, QProduct product) {
         BooleanBuilder builder = new BooleanBuilder();
 
         if (condition.getAuctionCategory() != null) {
             builder.and(auction.auctionCategory.eq(condition.getAuctionCategory()));
         }
-
         if (condition.getProductCategory() != null) {
             builder.and(product.category.eq(condition.getProductCategory()));
         }
-        // 현재 입찰가를 알 수 있게 변경해야함
         if (condition.getMinPrice() != null && condition.getMaxPrice() != null) {
             builder.and(auction.startingBid.between(condition.getMinPrice(), condition.getMaxPrice()));
         }
         if (condition.getStartTime() != null && condition.getEndTime() != null) {
             builder.and(auction.endTime.between(condition.getStartTime(), condition.getEndTime()));
         }
-
         if (condition.getAuctionStatus() != null) {
             builder.and(auction.auctionStatus.eq(condition.getAuctionStatus()));
         }
 
-        return queryFactory.selectFrom(auction)
-            .leftJoin(auction.product, product) // Auction과 Product 조인
-            .where(builder)
-            // orderBy는 성능 이슈로 일단 보류
-            // .orderBy(auction.endTime.asc())
-            .fetch();
+        return builder;
     }
 
-    @Deprecated
-    @Override
-    public List<Auction> getAuctionsEndWith24Hours(LocalDateTime now, LocalDateTime next24Hours, AuctionStatus auctionStatus) {
-        QAuction auction = QAuction.auction;
-
-        BooleanExpression endTimeBetween = auction.endTime.between(now, next24Hours);
-        BooleanExpression statusEquals = auction.auctionStatus.eq(auctionStatus);
-
-        return queryFactory
-            .selectFrom(auction)
-            .where(endTimeBetween.and(statusEquals))
-            .orderBy(auction.endTime.asc())
-            .fetch();
+    private OrderSpecifier<?> getSortOrder(AuctionRequest.SearchCondition condition, QAuction auction) {
+        if (condition.getSortBy() != null) {
+            return switch (condition.getSortBy()) {
+                case "bidsAsc" -> auction.bids.size().asc();
+                case "bidsDesc" -> auction.bids.size().desc();
+                case "endingSoon" -> auction.endTime.asc();
+                default -> null;
+            };
+        }
+        return null;
     }
-
-
 }
