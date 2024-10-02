0564b41ba118c1fa73d008103b0520218fc4d4aa
knu-k <kang4030@knu.ac.kr>
2024-10-02 23:10:18 +0900

    config : test 용 initializer 구성 (Auction 초기 만들기)

---
+++ src/main/java/com/tasksprints/auction/common/TableInitializer/TableInitializer.java
@@ -0,0 +1,12 @@
+package com.tasksprints.auction.common.TableInitializer;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TableInitializer implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
